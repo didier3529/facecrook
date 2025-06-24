3b2633242e2ca6d6d59255a46eb93aada3ec1f96
Didier <didiertoken@mail.com>
2025-06-24 12:43:25 +1000

    test: add App render test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,1 +1,15 @@
- 
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+test('App renders without crashing', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
+  // ensure a piece of UI is in the document
+  expect(screen.getByText(/Create Your Fake Crypto Persona/i)).toBeInTheDocument();
+});
